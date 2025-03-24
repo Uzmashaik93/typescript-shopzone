86b2cc6e8f9336cf5def553ebf82e4450ff5a620
uzma shaik <uzma97910@gmail.com>
2025-03-24 21:10:25 +0100

    feat:initial clean up

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <div className='logo'>
+        <img src="https://pngimg.com/d/amazon_PNG27.png" alt=""/>
+        <h2>Shopzone</h2>
+      </div>
+       
+        <input type="text" placeholder='search the product'/>
+    </header>
+  )
+}
+
+export default Header
